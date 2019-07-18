7121c456b93e3269de3ca573ae1d32f1290a8b92
Trey Dockendorf <treydock@gmail.com>
2019-07-18 17:05:58 -0400

    Simplify acceptance tests setup

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,9 +4,8 @@ require 'beaker/module_install_helper'
 require 'beaker/puppet_install_helper'
 
 run_puppet_install_helper
-proj = File.join(File.dirname(__FILE__), '..')
-modulepath = '/etc/puppetlabs/code/modules'
-copy_module_to(hosts, :source => proj, :module_name => 'yum', :target_module_path => modulepath)
+install_module_dependencies
+install_module
 
 UNSUPPORTED_PLATFORMS = ['Suse','AIX','Solaris']
 
@@ -15,11 +14,4 @@ RSpec.configure do |c|
   c.formatter = :documentation
   c.add_setting :yum_full, default: false
   c.yum_full = (ENV['BEAKER_yum_full'] == 'yes' || ENV['BEAKER_yum_full'] == 'true')
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module dependencies
-    on hosts, puppet('module', 'install', 'puppetlabs-apache', '--version', '">= 3.2.0 < 4.0.0"'), { :acceptable_exit_codes => [0,1], :run_in_parallel => true }
-    on hosts, puppet('module', 'install', 'puppetlabs-stdlib', '--version', '">= 4.25.0 < 6.0.0"'), { :acceptable_exit_codes => [0,1], :run_in_parallel => true }
-    on hosts, puppet('module', 'install', 'puppetlabs-concat', '--version', '">= 4.2.1 < 5.0.0"'), { :acceptable_exit_codes => [0,1], :run_in_parallel => true }
-  end
 end
